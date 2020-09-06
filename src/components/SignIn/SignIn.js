5e9ecd60501e390a0444c20351e13a1a899e25f5
Rian Mellotte <Alfred@MacBook-Pro-2.local>
2020-09-06 18:58:58 +0100

    updating to deployed server

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://git.heroku.com/infinite-refuge-16995.git/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
