81b46d17f93820c841e04736bc87689346059313
Rian Mellotte <Alfred@MacBook-Pro-2.local>
2020-09-08 16:24:40 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ onInputChange = (event) => {
 
 onSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://infinite-refuge-16995.herokuapp.com', {
+    fetch('https://git.heroku.com/infinite-refuge-16995.git', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -103,7 +103,7 @@ onSubmit = () => {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://infinite-refuge-16995.herokuapp.com', {
+        fetch('https://git.heroku.com/infinite-refuge-16995.git', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
