5e31d98f434bcc4a077975f017432a032253479f
Rian Mellotte <Alfred@MacBook-Pro-2.local>
2020-12-12 16:35:08 +0000

    fixing bounding box for multiple faces

--- src/App.js
+++ src/App.js
@@ -71,8 +71,8 @@ loadUser = (data) => {
 
 
 calculateFaceLocation = (data) => {
-  console.log(data.outputs[0].data.regions);
-  const clarifaiFace = data.outputs[0].data.regions.region_info.bounding_box;
+  console.log(data.outputs[0].data.regions.entries());
+  const clarifaiFace = data.outputs[0].data.regions.entires().region_info.bounding_box;
   const image = document.getElementById('inputimage');
   const width = Number(image.width);
   const height = Number(image.height);
